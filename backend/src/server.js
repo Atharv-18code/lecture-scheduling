ff5eb9874a0b545c037c8e9bf0ae846dc908cd73
Atharv Goythale <atharvgoythalel@gmail.com>
2026-09-03 17:44:28 +0530

    Init: first commit

---
+++ backend/src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Lecture Scheduling API is running"
+  });
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
